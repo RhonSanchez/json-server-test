3f7ce3f0a262bbe079c7651a9b589c98d1e80e2a
RhonSanchez <rhonaldsanchez98@gmail.com>
2023-05-13 10:34:24 -0400

    middlewares

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
-// const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults();
 
-// server.use(middlewares);
+server.use(middlewares);
 // Add this before server.use(router)
 server.use(
 	// Add custom route here if needed
